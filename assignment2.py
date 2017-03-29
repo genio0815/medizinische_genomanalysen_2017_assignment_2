e2fec0be18983e9b688d5e90b679e9a0444854fa
alex <ned_flanders@gmx.at>
2017-03-29 13:06:13 +0200

    done

--- assignment2.py
+++ assignment2.py
@@ -1,80 +1,57 @@
 #! /usr/bin/env python3
 
+# rearranged file structure, reduced functionality to what
+# is really needed; just do a single loop over file entries
+# ugly but working...
+
 import vcf
+import os
 
-__author__ = 'XXX'
-
+__author__ = 'Alexander.Bindeus'
 
 class Assignment2:
-    
     def __init__(self):
-        ## Check if pyvcf is installed
-        print("PyVCF version: %s" % vcf.VERSION)
-        
+        print("\nAssignment 2")
+        print(__author__)
+        print("using PyVCF version: %s" % vcf.VERSION)
 
-    def get_average_quality_of_son(self):
-        '''
-        Get the average PHRED quality of all variants
-        :return: 
-        '''    
-        print("TODO")
-        
-        
-    def get_total_number_of_variants_of_son(self):
-        '''
-        Get the total number of variants
-        :return: total number of variants
-        '''
-        print("TODO")
-    
-    
-    def get_variant_caller_of_vcf(self):
-        '''
-        Return the variant caller name
-        :return: 
-        '''
-        print("TODO")
-        
-        
-    def get_human_reference_version(self):
-        '''
-        Return the genome reference version
-        :return: 
-        '''
-        print("TODO")
-        
-        
-    def get_number_of_indels(self):
-        '''
-        Return the number of identified INDELs 
-        :return: 
-        '''
-        print("TODO")
-        
+        # hard coded input - do not care...
+        vcf_reader = vcf.Reader(open(
+            '/home/alex/Desktop/BioInformatik/SS_17/Medizinische_Genomanalysen/Assignments/'
+            'AmpliseqExome.20141120.NA24385.vcf', 'r'))
 
-    def get_number_of_snvs(self):
-        '''
-        Return the number of SNVs
-        :return: 
-        '''
-        print("TODO")
-        
-    def get_number_of_heterozygous_variants(self):
-        '''
-        Return the number of heterozygous variants
-        :return: 
-        '''
-        print("TODO")
-        
-    
-    def print_summary(self):
-        print("Print all results here")
-    
-        
+        count = 0
+        indelCount = 0
+        avQualy = 0
+        snpCount = 0
+        hetvarCount = 0
+
+        for record in vcf_reader:
+            count = count + 1
+            avQualy = avQualy + record.QUAL
+
+            if record.is_indel:
+                indelCount = indelCount + 1
+
+            if record.is_snp:
+                snpCount = snpCount + 1
+
+            if record.num_het == 1:
+                hetvarCount = hetvarCount + 1
+
+        print('\naverage PHRED score:\t', avQualy / count)
+        print('number of variants:\t', count)
+        print("reference version:\t{}"
+              .format(os.path.basename(vcf_reader.metadata["reference"]).strip(".fasta")))
+        print('variant caller:\t', vcf_reader.metadata['source'][0])
+        print('number of indels\t', indelCount)
+        print('number of snvs:\t', snpCount)
+        print('number of heterozygous variants\t', hetvarCount)
+
 if __name__ == '__main__':
-    print("Assignment 2")
     assignment1 = Assignment2()
-    assignment1.print_summary()
-    
+
+
+
     
 
